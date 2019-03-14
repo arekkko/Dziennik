9f7b682d4091913075ae4b711fe31b31c85265db
tokolorado <48475948+tokolorado@users.noreply.github.com>
2019-03-14 17:23:01 +0100

    TEST

--- template/sections/header.php
+++ template/sections/header.php
@@ -2,7 +2,7 @@
     <?php          
         if(isset($_SESSION) && $_SESSION['login'] == 1){
             echo "<p>Jesteś zalogowany jako {$user->get_user_name()}</p>"; 
-            echo '<a href="?logout=true">Wyloguj się</a>';    
+            
         }
     ?>
 
--- template/sections/menu.php
+++ template/sections/menu.php
@@ -25,7 +25,8 @@
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-success my-2 mr-sm-2" type="submit">Search</button>
+      <a class="btn btn-outline-success my-2 mr-sm-2" href="?logout=true">Wyloguj się</a>   
     </form>
   </div>
 </nav>
