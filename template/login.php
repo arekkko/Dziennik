9f3f67535b6caa168c80694d21909647634e1c52
tokolorado <48475948+tokolorado@users.noreply.github.com>
2019-03-18 20:23:43 +0100

    Zmiany w stylu/Dodanie img
    img/styl

--- template/login.php
+++ template/login.php
@@ -2,13 +2,22 @@
     include 'sections/head.php';
     include 'sections/header.php';
 ?>
-  <h1 class="page-title">Zaloguj się</h1>
-  <form method="post" action="./?loginProcess">
-    <input type="text" placeholder="Nazwa użytkownika" name="login" />
-    <input type="password" name="password" />
-    <input type="submit" value="Zaloguj" />
-  </form>
-<span>Uczen - pmaz - adadadssassda@@</span><br>
-<span>Nauczyciel - akowalski - %@!RFGdsss</span>
+
+<div class="odstep"> </div>
+
+<div class="oknolog">
+    <div class="logo" >
+    <img class="logsrc" src="img/114.png">
+    </div>
+        <h1 class="page-title">Zaloguj się</h1>
+        <form method="post" action="./?loginProcess">
+          <input type="text" placeholder="Nazwa użytkownika" name="login" />
+          <input type="password" name="password" />
+          <input type="submit" value="Zaloguj" />
+        </form>
+      <span>Uczen - pmaz - adadadssassda@@</span><br>
+      <span>Nauczyciel - akowalski - %@!RFGdsss</span>
+
+</div>
 
 <?php include 'sections/footer.php'; ?>
--- template/sections/menu.php
+++ template/sections/menu.php
@@ -1,6 +1,6 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light nav-height">
-  <a class="navbar-brand" href="./">GreatBook</a>
+  <a class="navbar-brand" href="./"><img src="img/114.png"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
