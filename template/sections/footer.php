a77fbaaebbd501477ec43dae7aab71ab3c8520ec
tokolorado <48475948+tokolorado@users.noreply.github.com>
2019-03-19 12:50:37 +0100

    styl 19.03.19
    styl 19.03.19

--- template/sections/footer.php
+++ template/sections/footer.php
@@ -1,3 +1,4 @@
+</div>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/jQuery.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
--- template/sections/menu.php
+++ template/sections/menu.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light nav-height">
+<nav class="navbar navbar-expand-lg navbar-light bg-light nav-height ">
   <a class="navbar-brand" href="./"><img src="img/114.png"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -36,3 +36,4 @@
 
   </div>
 </nav>
+<div class="odstep1"> </div>
